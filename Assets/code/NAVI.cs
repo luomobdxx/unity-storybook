873e70f48c0d8408dc6a300032e1347172f00723
luomobdxx <163285081+luomobdxx@users.noreply.github.com>
2024-11-03 20:37:27 +0800

    Initial commit

---
+++ Assets/code/NAVI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NAVI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadMyScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void LoadMyScene(int scenenumber)
+    {
+        SceneManager.LoadScene(scenenumber);
+    }
+    void Start()
+
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
